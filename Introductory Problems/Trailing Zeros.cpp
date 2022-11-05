55843037d5a5fc43134bb7ad802f92b378c3797d
Yassine <yassine.h2j@gmail.com>
2022-11-05 10:53:48 +0100

    Introductory Problems

---
+++ Introductory Problems/Trailing Zeros.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long ans = 0, n, x=5;
+    cin >> n;
+    while(n>=x){
+        ans += n/x;
+        x *= 5;
+    }
+    cout<<ans;
+    return 0;
+}
