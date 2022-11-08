eae0be53249dadd3297d3e9b6e42a195803fdb06
Yassine <yassine.h2j@gmail.com>
2022-11-08 12:06:31 +0100

    Weird Algorithm

---
+++ Introductory Problems/Weird Algorithm.cpp
@@ -0,0 +1,20 @@
+//Solved by Saif Khelil
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long n  ;
+    cin >> n  ;
+    cout << n << " " ;
+    while ( n != 1 ) {
+        if( n%2 == 0 ) {
+            n = n/2 ;
+        }
+        else {
+            n = n*3+1 ;
+        }
+        cout << n << " " ;
+    }
+    return 0;
+}
